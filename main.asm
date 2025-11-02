dcd4d9b18abf7a9cbb1553033e652eca11845f3a
rickyringler <richard.rringler@gmail.com>
2025-11-02 14:44:26 -0600

    02112025

--- main.asm
+++ main.asm
@@ -1,8 +1,3 @@
-; https://www.man7.org/linux/man-pages/man2/brk.2.html
-; https://stackoverflow.com/questions/6988487/what-does-the-brk-system-call-do
-; https://stackoverflow.com/questions/32793117/assembly-call-vs-jmp
-; https://www.felixcloutier.com/x86/lea
-
 section .data
     o_data        equ 0
     o_previous    equ 8
@@ -37,14 +32,14 @@ push:
 
 pop:
     mov rbx, [last_node]
-    mov r9, [last_node]
-    sub r9, r9                    ; should zero out if it wasnt a nullptr, like to the base of the heap
-    jnz not_last_node
+    mov r9, rbx
+    test r9, r9
+    jz not_last_node             ; nullptr, dont pop
     mov rdi, rbx
     call brk_new
     mov rax, [rbx+ o_previous]   ; dereference previous node's address
     mov [last_node], rax         ; update global
-    ret                          ; we'd either brk on the popped memory or something else would overwrite it
+    ret                          ; because we allocate the memory adjacent to [o_data], we could also overwrite the old data
 
 not_last_node:
     mov rax, 1                    ; arbitrary flag for attempt at nullptr on pop
